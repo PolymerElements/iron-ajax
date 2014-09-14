76bde89de6fa9eaf9824c5807ae6386c9ccfacb1
Peter Burns <rictic@google.com>
2014-09-14 08:40:49 -0700

    Add progress and loading attributes; includes docs, demo, and test.

--- tests/js/htmltests.js
+++ tests/js/htmltests.js
@@ -1,4 +1,5 @@
 htmlSuite('core-ajax', function() {
   htmlTest('html/core-ajax.html');
   htmlTest('html/core-ajax-response-and-error.html');
+  htmlTest('html/core-ajax-progress-and-loading.html');
 });
